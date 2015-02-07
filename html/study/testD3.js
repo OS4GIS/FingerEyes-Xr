45906dc94d07d6bf04c6c97f3f1014c8ad842dc5
Dip2K <hjkim@geoservice.co.kr>
2015-02-07 14:09:07 +0900

    Study D3

--- html/study/testD3.js
+++ html/study/testD3.js
@@ -1,36 +1,27 @@
-﻿var dataSet = [300, 130, 5, 60, 240];
+﻿var dataSet = [
+    [{ year: 2010, p: 18 }, { year: 2011, p: 22 }, { year: 2012, p: 30 }, { year: 2013, p: 40 }],
+    [{ year: 2010, p: 12 }, { year: 2011, p: 25 }, { year: 2012, p: 45 }, { year: 2013, p: 80 }],
+    [{ year: 2010, p: 10 }, { year: 2011, p: 15 }, { year: 2012, p: 20 }, { year: 2013, p: 25 }]
+];
+
+var svgHeight = 240;
+var barWidth = 50;
+var step = 80;
+var offsetX = 10;
+var color = ["red", "pink", "orange"];
+var stack = d3.layout.stack().y(function (d) { return d.p; });
 
 d3.select("#myGraph")
+    .selectAll("g")
+    .data(stack(dataSet))
+    .enter()
+    .append("g")
+    .attr("fill", function (d, i) { return color[i]; })
     .selectAll("rect")
-    .data(dataSet)
+    .data(function (d, i) { return d; })
     .enter()
     .append("rect")
-    .attr("x", 0)
-    .attr("y", function (d, i) { return i * 25; })
-    .attr("width", "0px")
-    .attr("height", "20px")
-        .on("click", function () {
-            d3.select(this).style("fill", "cyan");
-        })
-    .transition()
-    .delay(function (d, i) {
-        return i * 500;
-    })
-    .duration(2000)
-    .attr("width", function (d, i) { return d + "px"; });
-
-d3.select("#updateButton").on("click",
-    function () {
-        for (var i = 0; i < dataSet.length; i++) {
-            dataSet[i] = Math.floor(Math.random() * 320);
-        }
-
-        d3.select("#myGraph")
-            .selectAll("rect")
-            .data(dataSet)
-            .attr("width",
-                function (d, i) {
-                    return d + "px";
-                })
-    }
-);
+    .attr("x", function (d, i) { return offsetX + i * step; })
+    .attr("y", function (d, i) { return svgHeight - d.y0 - d.y; })
+    .attr("width", barWidth)
+    .attr("height", function (d, i) { return d.y; });
